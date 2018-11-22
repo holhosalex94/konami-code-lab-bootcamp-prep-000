adf484b1a2253f0517805e151d8235bcb68d06c7
Alex Holhos <alex.holhos@emanuel.ro>
2018-11-22 12:54:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,17 +13,20 @@ const codes = [
 
 function init() {
   // your code here
-  var index = 0;
-  document.addEventListener('keydown', function (e) {
-    const key = parseInt(e.detail || e.which);
-     if (key === code[index]) {
-      index++;
-       if (index === code.length - 1) {
-        window.alert("YOU DID IT!");
-        index = 0;
+  var index = 0
+  document.body.addEventListener('keydown', onKeyDownHandler);
+   function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+    console.log('key', key)
+    if (key === code[index]) {
+      index++
+       if (index === code.length) {
+        alert("YOU DID IT!")
+        index = 0
       }
     } else {
-      index = 0;
+      index = 0
     }
-  }, false);
-}
+  }
+}	}
+ init();
