3267cd7d18f20e126c0c0779f122f216078bf281
Alex Holhos <alex.holhos@emanuel.ro>
2018-11-22 12:59:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -18,8 +18,14 @@ function init() {
    function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which)
     console.log('key', key)
-    for (let i = 0, l = codes.length; i < l; i++) {
-     alert("Hurray!")
+    if (key === codes[index]) {
+      index++
+       if (index === codes.length) {
+        alert("Hurray!")
+        index = 0
+      }
+    } else {
+      index = 0
     }
   }
 }	
